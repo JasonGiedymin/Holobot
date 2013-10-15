6b77b694ecc2908812f46f589982f62fc3d23fb9
Jason Giedymin <jasong@apache.org>
2013-10-14 22:47:53 -0400

    Fedora19 + openshift image + openshift scripts
    - Add Fedora19 Image
      - Add openshift scripts to `scripts/deploy/`
        - Add `install_open_shift.sh` shell script
        - Add `configure_origin.pp` puppet script

---
+++ scripts/deploy/configure_origin.pp
@@ -0,0 +1,49 @@
+# configure_origin.pp
+class { 'openshift_origin' :
+    #The DNS resolvable hostname of this host
+    node_fqdn                  => "broker.localhost.localdomain",
+
+    #The domain under which application should be created. Eg: <app>-<namespace>.example.com
+    cloud_domain               => 'localhost.localdomain',
+
+    #Upstream DNS server.
+    # EC2 use: 172.16.0.23
+    dns_servers                => ['8.8.8.8'],
+
+    enable_network_services    => true,
+    configure_firewall         => true,
+    configure_ntp              => true,
+
+    #Configure the required services
+    configure_activemq         => true,
+    configure_mongodb          => true,
+    configure_named            => true,
+    configure_avahi            => false,
+    configure_broker           => true,
+    configure_node             => true,
+
+    #Enable development mode for more verbose logs
+    development_mode           => true,
+
+    #Update the nameserver on this host to point at Bind server
+    update_network_dns_servers => true,
+
+    #Use the nsupdate broker plugin to register application
+    broker_dns_plugin          => 'nsupdate',
+
+    #If installing from a local build, specify the path for Origin RPMs
+    #install_repo               => 'file:///root/origin-rpms',
+
+    #If using BIND, let the broker know what TSIG key to use
+    named_tsig_priv_key         => 'JiLMW6kfKPxOQLt1+eR7II5q0KosEQ==',
+
+    #If using an external ethernet device other than eth0
+    #eth_device                 => '<ethernet device name, eg: enp0s5>',
+
+    #If using with GDM, or have users with UID 500 or greater, add to this list
+    #os_unmanaged_users         => ['gdm'],
+
+    #If using the stable version instead of the nightly
+    #install_repo               => 'release',
+    #dependencies_repo          => 'release',
+}
---
+++ scripts/deploy/install_open_shift.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+sudo yum upgrade -y
+sudo yum install -y --nogpgcheck http://mirror.us.leaseweb.net/epel/6/i386/epel-release-6-8.noarch.rpm
+sudo yum install -y --nogpgcheck http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-7.noarch.rpm
+
+# /etc/yum.repos.d/puppetlabs.repo
+# add:
+#   exclude=*mcollective*
+
+sudo yum install -y puppet facter tar bind bind-utils
+
+puppetModuleDir=/etc/puppet/modules
+if [ ! -d $puppetModuleDir ]; then
+  echo "Creating dir [$puppetModuleDir]..."
+  sudo mkdir $puppetModuleDir
+fi
+
+sudo puppet module install openshift/openshift_origin
+
+os_domain="localdomain"
+sudo rm -f /var/named/K$os_domain.*
+sudo /usr/sbin/dnssec-keygen -a HMAC-MD5 -b 512 -n USER -r /dev/urandom -K /var/named $os_domain
+temp_key=$(cat /var/named/K$os_domain.*.key  | awk '{print $8}')
+
+printf "\n---dnssec key---\n$temp_key\n------\n\n"
+
+# cat<<EOF>/etc/sysconfig/network
+# NETWORKING=yes
+# HOSTNAME=broker.$os_domain
+# EOF
+# echo "broker.$os_domain" > /etc/hostname
+# hostname broker.$os_domain
+
+
+# oo-register-dns --with-node-hostname broker --with-node-ip 192.168.3.19 --domain $os_name
